b2b6841c4ad4529b19f398e0b9726d94799bcdeb
unknown <marco.campos001@gmail.com>
2019-03-26 23:05:40 -0400

    switching urllibs2 to requests

--- api_request.py
+++ api_request.py
@@ -1,5 +1,5 @@
-#Credit to Paul's Geek Dad Blog
-#url: http://pdwhomeautomation.blogspot.com/2016/01/fitbit-api-access-using-oauth20-and.html
+# Credit to Paul's Geek Dad Blog
+# url: http://pdwhomeautomation.blogspot.com/2016/01/fitbit-api-access-using-oauth20-and.html
 
 import base64
 import urllib2
@@ -7,149 +7,150 @@ import urllib
 import sys
 import json
 import os
+import pprint
+import requests
+from requests import HTTPError
 
-#This is the Fitbit URL to use for the API call
+# This is the Fitbit URL to use for the API call
 FitbitURL = "https://api.fitbit.com/1/user/-/profile.json"
-
-#Use this URL to refresh the access token
+# Use this URL to refresh the access token
 TokenURL = "https://api.fitbit.com/oauth2/token"
-
-#Get and write the tokens from here
+# Get and write the tokens from here
 IniFile = "./tokens.txt"
-
-#From the developer site
+# From the developer site
 OAuthTwoClientID = "22D8Y4"
 ClientOrConsumerSecret = "c577dc3f1bfddfeb28da655892cf5c70"
 
-#Some contants defining API error handling responses
+# Some contants defining API error handling responses
 TokenRefreshedOK = "Token refreshed OK"
 ErrorInAPI = "Error when making API call that I couldn't handle"
 
-#Get the config from the config file.  This is the access and refresh tokens
+# Get the config from the config file.  This is the access and refresh tokens
 def GetConfig():
     print "Reading from the config file"
     
-    #Open the file
+    # Open the file
     FileObj = open(IniFile,'r')
     
-    #Read first two lines - first is the access token, second is the refresh token
+    # Read first two lines - first is the access token, second is the refresh token
     AccToken = FileObj.readline()
     RefToken = FileObj.readline()
     
-    #Close the file
+    # Close the file
     FileObj.close()
     
-    #See if the strings have newline characters on the end.  If so, strip them
+    # See if the strings have newline characters on the end.  If so, strip them
     if (AccToken.find("\n") > 0):
         AccToken = AccToken[:-1]
     if (RefToken.find("\n") > 0):
         RefToken = RefToken[:-1]
     
-    #Return values
+    # Return values
     return AccToken, RefToken
 
 def WriteConfig(AccToken,RefToken):
     print "Writing new token to the config file"
     print "Writing this: " + AccToken + " and " + RefToken
     
-    #Delete the old config file
+    # Delete the old config file
     os.remove(IniFile)
     
-    #Open and write to the file
+    # Open and write to the file
     FileObj = open(IniFile,'w')
     FileObj.write(AccToken + "\n")
     FileObj.write(RefToken + "\n")
     FileObj.close()
 
-#Make a HTTP POST to get a new
+
+# Make a HTTP POST to get a new
 def GetNewAccessToken(RefToken):
     print "Getting a new access token"
-    
-    #Form the data payload
+    # Form the data payload
     BodyText = {'grant_type' : 'refresh_token',
                 'refresh_token' : RefToken}
-    #URL Encode it
+    # URL Encode it
     BodyURLEncoded = urllib.urlencode(BodyText)
     print "Using this as the body when getting access token >>" + BodyURLEncoded
-    
-    #Start the request
+
+    # Start the request
     tokenreq = urllib2.Request(TokenURL,BodyURLEncoded)
-    
-    #Add the headers, first we base64 encode the client id and client secret with a : inbetween and create the authorisation header
+
+    # Add the headers, first we base64 encode the client id and client secret
+    # with a : inbetween and create the authorisation header
     tokenreq.add_header('Authorization', 'Basic ' + base64.b64encode(OAuthTwoClientID + ":" + ClientOrConsumerSecret))
     tokenreq.add_header('Content-Type', 'application/x-www-form-urlencoded')
-    
-    #Fire off the request
+
+    # Fire off the request
     try:
         tokenresponse = urllib2.urlopen(tokenreq)
-        
-        #See what we got back.  If it's this part of  the code it was OK
+
+        # See what we got back.  If it's this part of  the code it was OK
         FullResponse = tokenresponse.read()
-        
-        #Need to pick out the access token and write it to the config file.  Use a JSON manipluation module
+
+        # Need to pick out the access token and write it to the config file.  Use a JSON manipluation module
         ResponseJSON = json.loads(FullResponse)
-        
-        #Read the access token as a string
+
+        # Read the access token as a string
         NewAccessToken = str(ResponseJSON['access_token'])
         NewRefreshToken = str(ResponseJSON['refresh_token'])
-        #Write the access token to the ini file
+        # Write the access token to the ini file
         WriteConfig(NewAccessToken,NewRefreshToken)
-        
+
         print "New access token output >>> " + FullResponse
     except urllib2.URLError as e:
-        #Getting to this part of the code means we got an error
+        # Getting to this part of the code means we got an error
         print "An error was raised when getting the access token.  Need to stop here"
         print e.code
         print e.read()
         sys.exit()
 
-#This makes an API call.  It also catches errors and tries to deal with them
+# This makes an API call.  It also catches errors and tries to deal with them
 def MakeAPICall(InURL,AccToken,RefToken):
-    #Start the request
+    # Start the request
     req = urllib2.Request(InURL)
-    
-    #Add the access token in the header
-    req.add_header('Authorization', 'Bearer ' + AccToken)
-    
-    print "I used this access token " + AccToken
-    #Fire off the request
+    # url = "http://httpbin.org/status/404"
+    headerValue = {
+        'Authorization': 'Bearer ' + AccToken
+    }
     try:
-        #Do the request
-        response = urllib2.urlopen(req)
-        #Read the response
-        FullResponse = response.read()
-        
-        #Return values
-        return True, FullResponse
-    #Catch errors, e.g. A 401 error that signifies the need for a new access token
-    except urllib2.URLError as e:
-        print "Got this HTTP error: " + str(e.code)
-        HTTPErrorMessage = e.read()
-        print "This was in the HTTP error message: " + HTTPErrorMessage
-        #See what the error was
-        if (e.code == 401) and (HTTPErrorMessage.find("Access token invalid or expired") > 0):
+        request = requests.get(InURL, headers=headerValue)
+        request.raise_for_status()
+
+        # Add the access token in the header
+        req.add_header('Authorization', 'Bearer ' + AccToken)
+        print("Result code: {0}".format(request.status_code))
+        print("Returned data: \n{0}".format(request.content))
+
+        # print "I used this access token " + AccToken
+        # response = urllib2.urlopen(req)
+        # FullResponse = response.read()
+        return True, request
+    except HTTPError as err: #urllib2.URLError as e:
+        print("Error: {0}".format(err))#HTTP error: " + str(e.code)
+        if err.response.status_code == 401:
             GetNewAccessToken(RefToken)
             return False, TokenRefreshedOK
-        #Return that this didn't work, allowing the calling function to handle it
         return False, ErrorInAPI
 
-#Main part of the code
-#Declare these global variables that we'll use for the access and refresh tokens
+# Main part of the code
+# Declare these global variables that we'll use for the access and refresh tokens
 AccessToken = ""
 RefreshToken = ""
 
 print "Fitbit API Test Code"
 
-#Get the config
+# Get the config
 AccessToken, RefreshToken = GetConfig()
+MakeAPICall(FitbitURL, AccessToken, RefreshToken)
 
-#Make the API call
-APICallOK, APIResponse = MakeAPICall(FitbitURL, AccessToken, RefreshToken)
-
-if APICallOK:
-    print APIResponse
-else:
-    if (APIResponse == TokenRefreshedOK):
-        print "Refreshed the access token.  Can go again"
-    else:
-        print ErrorInAPI
+# # Make the API call
+# APICallOK, APIResponse = MakeAPICall(FitbitURL, AccessToken, RefreshToken)
+# if APICallOK:
+#     # print(json.dumps(APIResponse, indent=4))
+#     # pprint.pformat(APIResponse)#, indent=4)
+#     print(APIResponse)
+# else:
+#     if (APIResponse == TokenRefreshedOK):
+#         print "Refreshed the access token.  Can go again"
+#     else:
+#         print ErrorInAPI
--- create_token.py
+++ create_token.py
@@ -1,5 +1,5 @@
-#Credit to Paul's Geek Dad Blog
-#url: http://pdwhomeautomation.blogspot.com/2016/01/fitbit-api-access-using-oauth20-and.html
+# Credit to Paul's Geek Dad Blog
+# url: http://pdwhomeautomation.blogspot.com/2016/01/fitbit-api-access-using-oauth20-and.html
 
 import base64
 import urllib2
@@ -7,34 +7,34 @@ import urllib
 import os
 import json
 
-#These are the secrets etc from Fitbit developer
+# These are the secrets etc from Fitbit developer
 OAuthTwoClientID = "22D8Y4"
 ClientOrConsumerSecret = "c577dc3f1bfddfeb28da655892cf5c70"
 
-#This is the Fitbit URL
+# This is the Fitbit URL
 TokenURL = "https://api.fitbit.com/oauth2/token"
 
-#I got this from the first verifier part when authorising my application
-AuthorisationCode = ""
+# I got this from the first verifier part when authorising my application
+AuthorisationCode = "8d703f8f8025210d39dfa720af71d1d233df8aba"
 
-#Form the data payload
-BodyText = {'code' : AuthorisationCode,
-            'redirect_uri' : 'https://lvlarco.github.io/projects/hikes/hikes',
-            'client_id' : OAuthTwoClientID,
-            'grant_type' : 'authorization_code'}
+# Form the data payload
+BodyText = {'code': AuthorisationCode,
+            'redirect_uri': 'https://lvlarco.github.io/projects/hikes/hikes',
+            'client_id': OAuthTwoClientID,
+            'grant_type': 'authorization_code'}
 
 BodyURLEncoded = urllib.urlencode(BodyText)
 print BodyURLEncoded
 
-#Start the request
+# Start the request
 req = urllib2.Request(TokenURL,BodyURLEncoded)
 
-#Add the headers, first we base64 encode the client id and client secret with a : inbetween and create 
-#the authorisation header
+# Add the headers, first we base64 encode the client id and client secret with a : inbetween and create
+# the authorisation header
 req.add_header('Authorization', 'Basic ' + base64.b64encode(OAuthTwoClientID + ":" + ClientOrConsumerSecret))
 req.add_header('Content-Type', 'application/x-www-form-urlencoded')
 
-#Fire off the request
+# Fire off the request
 try:
     response = urllib2.urlopen(req)
 
